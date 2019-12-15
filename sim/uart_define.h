ec65471d9e49278ec318dab663a847b85c7f661e
nguyenquanicd <nguyenquan.icd@gmail.com>
2019-12-15 10:16:48 +0700

    Create uart_define.h

---
+++ sim/uart_define.h
@@ -0,0 +1,9 @@
+//--------------------------------------
+//Project:  The UVM environemnt for UART (Universal Asynchronous Receiver Transmitter)
+//Module:   -
+//Function: All defines
+//Author:   Nguyen Hung Quan, Truong Cong Hoang Viet, Le Hoang Van
+//Page:     VLSI Technology
+//--------------------------------------
+#define DELAY 1
+//#define INTERRUPT_COM
